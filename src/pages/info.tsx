70bd7ac6b5d3818694dde1ec4209a6848a301483
elifedd <elif.demircioglu@icloud.com>
2023-11-11 19:41:23 +0300

    nft-marketplace :: add contract info page

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,16 @@
+import ContractMetadata from "@/components/ContractMetadata";
 import Layout from "@/layout/Layout";
+import { Metadata } from "@/types/metadata";
+import { getMarketplaceContract, getNFTContract } from "@/util/getContracts";
+import { useContractMetadata } from "@thirdweb-dev/react";
 
-export default function Info() {
+const InfoPage = () => {
+    const {marketplace} = getMarketplaceContract();
+    const {nft_contract} = getNFTContract();
+
+    const {data:marketplace_metadata, isLoading:marketMetadataLoading} = useContractMetadata(marketplace);
+    const {data:nft_metadata, isLoading:nftMetadataLoading} = useContractMetadata(nft_contract);
+
     return (
         <Layout>
             <div>
@@ -8,6 +18,28 @@ export default function Info() {
                     Contract Details
                 </h1>
             </div>
+
+            {nftMetadataLoading || marketMetadataLoading && (
+                <div className="text-">
+                    Loading contract info...
+                </div>
+            )}
+
+            {marketplace_metadata && (
+                <ContractMetadata 
+                    metadata={marketplace_metadata as Metadata} 
+                    title="NFT Marketplace Contract Metadata"
+                />
+            )}
+
+            {nft_metadata && (
+                <ContractMetadata 
+                    metadata={nft_metadata as Metadata} 
+                    title="NFT Collection Contract Metadata"
+                />
+            )}
         </Layout>
     );
-}
+};
+
+export default InfoPage;
